f0ce868c5d32206884dbe7ac69355dacd788afde
Sabith <masterdevsabith@gmail.com>
2025-08-12 21:22:44 +0530

    tried bubble sort

---
+++ arrays/bubble_sort.py
@@ -0,0 +1,16 @@
+my_array = [100, 2000, 300, 140, 150, 2]
+
+n = len(my_array)
+
+for i in range(n-1):
+    swapped = False
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+            swapped = True
+
+    if not swapped:
+        break
+
+
+print("sorted array is :  ", my_array)
